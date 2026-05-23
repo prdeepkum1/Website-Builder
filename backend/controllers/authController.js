8f47d78bef302e3c88c3900c85bb0583bcf00f23
Pradeep kumar <pradeepk48842@gmail.com>
2026-05-23 14:09:24 +0530

    Update cookie settings for token in authController

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -15,7 +15,7 @@ export const googleAuth = async(req, res)=>{
             {expiresIn:"7d"}
         )
         res.cookie("token", token, 
-            { httpOnly: true, secure:false, sameSite:"lax", maxAge:7*24*60*60*1000})
+            { httpOnly: true, secure:true, sameSite:"none", maxAge:7*24*60*60*1000})
             return res.status(200).json(user)
         
 
